ffe81bd22f62b2ffce49574b842873f601477b91
Eric Castoldi <ericgtr17@gmail.com>
2016-09-11 18:01:23 -0300

    Refatoraçoes e criação do Article.jsx

--- src/store/components/Landing.jsx
+++ src/store/components/Landing.jsx
@@ -2,8 +2,16 @@ var React = require('react');
 var Menu = require('./Menu.jsx');
 var Logo = require('./Logo.jsx');
 var UnderstandButton = require('./UnderstandButton.jsx');
+var Subscribe = require('./Subscribe.jsx');
+var SubscribePopup = require('./SubscribePopup.jsx');
+
+var connect = require('react-redux').connect;
 
 var Landing = React.createClass({
+
+  propTypes: {
+    subscribePopupOn: React.PropTypes.bool.isRequired
+  },
 
   render: function () {
 
@@ -14,9 +22,17 @@ var Landing = React.createClass({
           <Logo />
           <UnderstandButton />
         </div>
+        <Subscribe />
+        <SubscribePopup active={this.props.subscribePopupOn} />
       </div>
     );
   }
 });
 
-module.exports = Landing;
+function mapStateToProps(state) {
+  return {
+    subscribePopupOn: state.main.subscribePopupOn
+  };
+}
+
+module.exports = connect(mapStateToProps)(Landing);
--- src/store/components/SafieStore.jsx
+++ src/store/components/SafieStore.jsx
@@ -1,39 +1,20 @@
 var React = require('react');
-var connect = require('react-redux').connect;
-
 var Footer = require('./Footer.jsx');
-var Subscribe = require('./Subscribe.jsx');
-var SubscribePopup = require('./SubscribePopup.jsx');
 
 var SafieStore = React.createClass({
 
   propTypes: {
-    subscribePopupOn: React.PropTypes.bool.isRequired,
     children: React.PropTypes.node.isRequired
   },
 
   render: function () {
     return (
       <div className="safie-store">
-
         {this.props.children}
-
-        <Subscribe />
-
         <Footer />
-
-        <SubscribePopup active={this.props.subscribePopupOn} />
-
       </div>
     );
   }
 });
 
-function mapStateToProps(state) {
-  return {
-    subscribePopupOn: state.main.subscribePopupOn
-  };
-}
-
-
-module.exports = connect(mapStateToProps)(SafieStore);
+module.exports = SafieStore;
