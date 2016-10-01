e5cc3623d8a219bb6f47e7de8d237bc892f43d2d
Eric Castoldi <ericgtr17@gmail.com>
2016-10-01 17:52:47 -0300

    Implementação das ações de navegação nas fotos

--- src/store/components/ProductPicturesViewer.jsx
+++ src/store/components/ProductPicturesViewer.jsx
@@ -6,14 +6,36 @@ var ProductPicturesViewer = React.createClass({
 
   getInitialState: function (){
     return {
-      activePicture: '/img/demo/lookbook13.jpg',
-      pictures: [
-        '/img/demo/lookbook13.jpg',
-        '/img/demo/lookbook14.jpg',
-        '/img/demo/lookbook15.jpg',
-        '/img/demo/detalhe-saia.jpg'
-      ]
+      activePicture: 1,
+      pictures: {
+        1: '/img/demo/lookbook13.jpg',
+        2: '/img/demo/lookbook14.jpg',
+        3: '/img/demo/lookbook15.jpg',
+        4: '/img/demo/detalhe-saia.jpg'
+      }
     };
+  },
+
+  nextPicture: function(){
+    if(Number(this.state.activePicture) === Object.keys(this.state.pictures).length)
+    {
+      this.changeActivePicture(1);
+      return;
+    }
+
+    var next = Number(this.state.activePicture) + 1;
+    this.changeActivePicture(next);
+  },
+
+  previousPicture: function(){
+    if(Number(this.state.activePicture) === 1)
+    {
+      this.changeActivePicture(Object.keys(this.state.pictures).length);
+      return;
+    }
+
+    var next = Number(this.state.activePicture) - 1;
+    this.changeActivePicture(next);
   },
 
   changeActivePicture: function(pic){
@@ -23,6 +45,8 @@ var ProductPicturesViewer = React.createClass({
 
   render: function(){
 
+    var activePicturePath = this.state.pictures[this.state.activePicture];
+
     return (
       <div className="picture-viewer">
         <div className="container">
@@ -30,13 +54,14 @@ var ProductPicturesViewer = React.createClass({
             <div className="three columns">
               <ProductPicturesPicker
                 pictures={this.state.pictures}
+                activePicture={this.state.activePicture}
                 picturePicked={this.changeActivePicture} />
             </div>
             <div className="nine columns">
               <div className="foto-principal-produto">
-                <i className="fa fa-angle-double-left fa-2" aria-hidden="true"></i>
-                <ProductPicture picture={this.state.activePicture} />
-                <i className="fa fa-angle-double-right fa-2" aria-hidden="true"></i>
+                <i onClick={this.previousPicture} className="fa fa-angle-double-left fa-2" aria-hidden="true"></i>
+                <ProductPicture picture={activePicturePath} />
+                <i onClick={this.nextPicture} className="fa fa-angle-double-right fa-2" aria-hidden="true"></i>
               </div>
             </div>
           </div>
